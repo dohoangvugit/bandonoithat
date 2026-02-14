f0e9b6e9d7c84e8ba3759ed838d6b4610c44d375
Đỗ Hoàng Vũ <vuker12345@gmail.com>
2026-02-15 01:14:33 +0700

    thêm toast cho detail

--- src/public/js/details.js
+++ src/public/js/details.js
@@ -1,17 +1,29 @@
-let quantity = 1;
+let quantity = 1
+let cart = []
 
 function increment() {
-  quantity++;
-  document.getElementById("qty").innerText = quantity;
+  quantity++
+  document.getElementById("qty").innerText = quantity
 }
 
 function decrement() {
-  if (quantity === 1) return;
-  quantity--;
-  document.getElementById("qty").innerText = quantity;
+  if (quantity === 1) return
+  quantity--
+  document.getElementById("qty").innerText = quantity
 }
 
 function addToCart(product) {
-  product.quantity = quantity;
-  console.log("ADD TO CART:", product);
+  const index = cart.findIndex(item => item.id === product.id)
+
+  if (index >= 0) {
+    cart[index].quantity += quantity
+  } else {
+    cart.push({ ...product, quantity })
+  }
+
+  console.log("Cart:", cart)
+
+  // SHOW TOAST
+  const toast = new bootstrap.Toast(document.getElementById("cartToast"))
+  toast.show()
 }
